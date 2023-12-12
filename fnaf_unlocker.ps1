40b89ad4cdb5b740173a54cd010339520d9bff0e
Jotaxisz <96952261+LunzRh@users.noreply.github.com>
2023-12-12 12:47:44 -0300

    Update fnaf_unlocker.ps1

--- fnaf_unlocker.ps1
+++ fnaf_unlocker.ps1
@@ -1,18 +1,12 @@
-# Fnaf saves
 $files = @("freddy", "freddy2", "freddy3", "fn4", "sl")
-
-# Download fnaf save files B)
-foreach ($i in $files){
-	Invoke-WebRequest "https://github.com/LunzRh/Fnaf-save-cheat/tree/main/fnaf-saves/$i"
-}
 
 if (-NOT(Test-Path $env:appdata\MMFApplications)){
 	Write-Output " > Creating MMFApplications directory ..."
 	New-Item -Path $env:appdata\MMFApplications -ItemType Directory
 }
 
-foreach ($j in $files){
-	Move-Item $j $env:appdata\MMFApplications\ -Force
+foreach ($i in $files){
+	Invoke-WebRequest -Uri "https://github.com/LunzRh/Fnaf-save-cheat/tree/main/fnaf-saves/$i" -OutFile $i
+	Move-Item $i $env:appdata\MMFApplications\ -Force
 }
 Write-Output " > Done."
-
