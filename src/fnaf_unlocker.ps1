81825e147df7193ff4c6a33356ce9e853ed3cd0f
Jotaxisz <96952261+LunzRh@users.noreply.github.com>
2024-02-17 19:16:16 -0300

    Add files via upload

---
+++ src/fnaf_unlocker.ps1
@@ -0,0 +1,13 @@
+$files = @("freddy", "freddy2", "freddy3", "fn4", "sl")
+
+if (-NOT(Test-Path $env:appdata\MMFApplications)){
+	Write-Output " > Creating MMFApplications directory ..."
+	New-Item -Path $env:appdata\MMFApplications -ItemType Directory
+}
+
+foreach ($i in $files){
+    $url = "https://raw.githubusercontent.com/LunzRh/Fnaf-save-cheat/main/fnaf-saves/" + $i
+	Invoke-WebRequest -Uri $url -OutFile $i
+	Move-Item $i $env:appdata\MMFApplications\ -Force
+}
+Write-Output " > Done."
